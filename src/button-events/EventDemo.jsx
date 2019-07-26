4929bd752eb4a7baba0e4d9438337eec058fd53c
matthewzhangx <matthewzhangx@github.com>
2019-07-26 22:17:42 +0800

    Props done

--- src/button-events/EventDemo.jsx
+++ src/button-events/EventDemo.jsx
@@ -5,9 +5,11 @@ import { simpleAction } from '../redux/actions/simpleAction';
 class EventDemo extends React.Component {
   constructor(props) {
       super();
-      this.state = { count: 0 , newName: 'YangSB'};
+      this.state = { count: 0 , newName: 'YangSB', inputValue:''};
       this.myName = props.myName;
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleClear = this.handleClear.bind(this);
+      this.handleInput = this.handleInput.bind(this);
       console.log(props.myName);
 
       this.student = {
@@ -30,11 +32,29 @@ class EventDemo extends React.Component {
     }));
   }
 
+    handleClear() {
+        console.log(this.state.count);
+        this.setState(state => ({
+            count: 0,
+            newName: 'newName'
+        }));
+    }
+
+    handleInput(value) {
+        console.log(value);
+        this.setState(state => ({
+            inputValue: value,
+        }));
+    }
+
     render() {
       console.log(this.props.simpleReducer.reduxName);
       let a = this.student.name;
       return (
        <div>
+           <div>
+               this is my name given by my father: {this.props.myName}
+           </div>
          This is my Name: {a}
          <div>
          This is my newName: {this.state.newName}
@@ -45,10 +65,21 @@ class EventDemo extends React.Component {
          <div>
            This is a counter: {this.state.count}
          </div>
+           <div>
+               This is my input: {this.state.inputValue}
+           </div>
 
          <div>
           <button onClick={this.handleSubmit}> Plus </button>
          </div>
+
+           <div>
+               <button onClick={this.handleClear}> Clear </button>
+           </div>
+
+           <div>
+               <input onChange={event => {this.handleInput(event.target.value)}}/>
+           </div>
 
          <div>
           <button className={'btn-primary'} onClick={this.simpleAction}> Redux </button>
